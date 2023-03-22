52bbec82b21bda029c984cd0468f30098baf2b9d
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-03-22 13:34:34 +0100

    funzione di aggornamento

--- CRUD - 25/Form1.Designer.cs
+++ CRUD - 25/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.Nome = new System.Windows.Forms.Label();
+            this.Price = new System.Windows.Forms.TextBox();
+            this.Prezzo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(74, 79);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(100, 20);
+            this.Name.TabIndex = 0;
+            this.Name.TextChanged += new System.EventHandler(this.Name_TextChanged);
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(74, 60);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(35, 13);
+            this.Nome.TabIndex = 1;
+            this.Nome.Text = "Nome";
+            // 
+            // Price
+            // 
+            this.Price.Location = new System.Drawing.Point(283, 78);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(100, 20);
+            this.Price.TabIndex = 2;
+            this.Price.TextChanged += new System.EventHandler(this.Price_TextChanged);
+            // 
+            // Prezzo
+            // 
+            this.Prezzo.AutoSize = true;
+            this.Prezzo.Location = new System.Drawing.Point(283, 59);
+            this.Prezzo.Name = "Prezzo";
+            this.Prezzo.Size = new System.Drawing.Size(39, 13);
+            this.Prezzo.TabIndex = 3;
+            this.Prezzo.Text = "Prezzo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Prezzo);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.Name);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.TextBox Price;
+        private System.Windows.Forms.Label Prezzo;
     }
 }
 
--- CRUD - 25/Form1.cs
+++ CRUD - 25/Form1.cs
@@ -7,14 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CRUD___25
 {
     public partial class Form1 : Form
     {
+        #region declare
+        string file;
         public Form1()
         {
             InitializeComponent();
+            file = @"carrello.csv";
+        }
+        #endregion
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        #region pulsanti
+        private void Name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Price_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region funzioni di servizio
+        void upd(string n, string p)
+        {
+            if(!File.Exists(file))
+            {
+                using (StreamWriter sw = new StreamWriter(file, append: false))
+                {
+                    sw.WriteLine(n + " €" + p);
+                }
+                
+            }
+            else
+            {
+                using (StreamWriter sr = new StreamWriter(file))
+                {
+                    sr.WriteLine(n + " €" + p);
+                }
+            }
         }
     }
 }
